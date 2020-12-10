c02d819f1bc4556a14ac7949136fad42974b84c0
Roman_Ponomarenko1 <Roman_Ponomarenko1@epam.com>
2020-12-10 17:57:06 +0200

    fix integration tests #2

--- validator-service/src/component/java/com/component/ValidatorServiceComponentTest.java
+++ validator-service/src/component/java/com/component/ValidatorServiceComponentTest.java
@@ -20,7 +20,7 @@ public class ValidatorServiceComponentTest {
 
     @Test
     public void validateUserTest() {
-        User user = new User(1, "aaa", "aaa@aa.qq", true);
+        User user = new User(2L, "user", "user@ww.qq", true);
         ValidatedUser validatedUser = new ValidatedUser(true);
         assertThat(client.validateUser((long) 1, validatedUser)).isEqualTo(user);
     }
